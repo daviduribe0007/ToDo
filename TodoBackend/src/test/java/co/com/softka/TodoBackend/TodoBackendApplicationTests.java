85fce72f7c173cbd9fa57bd846b7fb1d8c54a41e
daviduribe0007 <daviduribe0007@gmail.com>
2021-02-23 09:02:02 -0500

    Make basic structure back and front for the project ToDo

---
+++ TodoBackend/src/test/java/co/com/softka/TodoBackend/TodoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.softka.TodoBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
